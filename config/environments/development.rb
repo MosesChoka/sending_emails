870787e9fd805645376340001b111427366f8f92
MC <chokamoses2@gmail.com>
2024-11-10 20:04:16 +0300

    configured action mailer with gmail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,11 +41,29 @@ Rails.application.configure do
   # caching is enabled.
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
   config.action_mailer.delivery_method = :sendmail
+# Defaults to:
+# config.action_mailer.sendmail_settings = {
+#   location: '/usr/sbin/sendmail',
+#   arguments: %w[ -i ]
+# }
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_options = {from: "test@example.com"}
+  config.action_mailer.default_options = { from: "no-reply@example.com" }
+
+  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:         "smtp.gmail.com",
+    port:            587,
+    domain:          "example.com",
+    user_name:       Rails.application.credentials.dig(:user_name),
+    password:        Rails.application.credentials.dig(:password),
+    authentication:  "plain",
+    enable_starttls: true,
+    open_timeout:    5,
+    read_timeout:    5 }
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
